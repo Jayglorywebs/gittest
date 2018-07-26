e3b2a1acb324092111eaebac640f6bba443b5674
Glorywebs Creatives Pvt. Ltd <33888243+Jayglorywebs@users.noreply.github.com>
2018-07-26 16:40:14 +0530

    first content

---
+++ adminemail.php
@@ -0,0 +1,162 @@
+ 
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Air Charter International</title>
+  
+    <style type="text/css">
+   .tableContent img{border:0!important;display:block!important;outline:0!important}a,h2,p{color:#382F2E}div,h2,p,ul{margin:0}a.link1{color:#E3523A;font-size:13px;font-weight:700;text-decoration:none}h2{color:#FFF;font-size:21px;font-weight:400}.bgBody{background:#d9d8d8}.bgItem{background:#333}li,p{color:#AAA;font-size:13px;line-height:19px}.contentEditable h2{text-transform:uppercase;color:#000;font-size:30px;padding-left:15px}.contentTextEditable p{color:#000;margin-bottom:5px;font-size:15px;text-transform:capitalize;line-height:25px;text-align:justify}.contentEditable h3{line-height:30px}.movableContent-footer span{color:#fff;font-size:12px;opacity:.8;letter-spacing:.5px}ul.bottom-social{margin:0;padding:0;list-style-type:none}.bottom-social li{display:inline-block;padding-right:15px}.bottom-social li a{color:#fff;font-size:15px}@media only screen and (max-width:480px){img[class=banner],table[class=MainContainer],td[class=cell]{width:100%!important;height:auto!important}td[class=specbundle]{width:100%!important;float:left!important;font-size:13px!important;line-height:17px!important;display:block!important;padding-bottom:20px!important}td[class=specbundle3]{width:20px!important;float:left!important;display:block!important;background-color:#f2f2f2!important}td[class=spechide]{display:none!important}td[class=left_pad]{padding-left:15px!important;padding-right:15px!important}}@media only screen and (max-width:540px){img[class=banner],table[class=MainContainer],td[class=cell]{width:100%!important;height:auto!important}td[class=specbundle]{width:100%!important;float:left!important;font-size:13px!important;line-height:17px!important;display:block!important;padding-bottom:20px!important}td[class=specbundle3]{width:20px!important;float:left!important;display:block!important;background-color:#f2f2f2!important}td[class=spechide]{display:none!important}td[class=left_pad]{padding-left:15px!important;padding-right:15px!important}}
+    </style>
+
+    <script type="colorScheme" class="swatch active">
+        { "name":"Default", "bgBody":"333333", "link":"E3523A", "color":"AAAAAA", "bgItem":"333333", "title":"FFFFFF" }
+
+    </script>
+
+</head>
+
+<body paddingwidth="0" paddingheight="0" offset="0" toppadding="0" leftpadding="0" class='bgBody'>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0" class="tableContent bgBody" align="center">
+        <tbody>
+            <tr>
+                <td>
+                    <table width="600" border="0" cellspacing="0" cellpadding="0" align="center" class="MainContainer" style='font-family:helvetica, sans-serif;'>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tbody>
+                                            <tr>
+                                                <td valign="top" width="20" class="spechide">&nbsp;</td>
+                                                <td class='movableContentContainer'>
+                                                    <div class="movableContent" style="border: 0px; padding-top: 0px; position: relative;">
+                                                        <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+
+                                                            <!--  =========================== The header ===========================  -->
+
+                                                            <!--  =========================== The body ===========================  -->
+                                                            <tr>
+                                                                <td height='25'>&nbsp;</td>
+                                                            </tr>
+
+                                                            <tr bgcolor='#efefef'>
+                                                                <td class="specbundle2" style="padding:15px;">
+                                                                    <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+                                                                        <tr>
+
+                                                                            <td align='left' valign='middle' width="33%">
+                                                                                <div class="contentEditableContainer contentImageEditable">
+                                                                                    <div class="contentEditable">
+                                                                                        <a href="http://aircharter-international.com"><img style="    max-height: 90px;" src="https://www.aircharter-international.com/wp-content/themes/aircharteri/images/air-charter-international-logo.png" alt="logo"></a>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </td>
+
+                                                                            <td align='right' valign='middle'>
+                                                                                <div class="contentEditableContainer contentTextEditable" style='display:inline-block;'>
+                                                                                    <div class="contentEditable">
+                                                                                        <p>phone: <a href="tel:+97148070707" >+971 4 807 0707</a></p>
+                                                                                        <p>Email: <a href="mailto:sales@aircharter-international.com" style="    text-transform: lovercase;">sales@aircharter-international.com</a></p>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </td>
+
+
+                                                                        </tr>
+                                                                    </table>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </div>
+
+                                                    <div class="movableContent" style="border: 0px; padding-top: 0px; position: relative;">
+                                                        <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" style="padding: 15px;background: #fff; border: 0;">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td style="border: 0;">
+                                                                        <h3 style="text-transform: capitalize">Hello Admin,</h3>
+                                                                    </td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td style="border: 0;padding-bottom: 15px;">
+                                                                        <p style="color: #000000; font-size: 15px">There is New Request placed on <strong><a href="http://aircharter-international.com" style="text-decoration:none;">Air Charter International</a>.</strong></p>
+                                                                    </td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td style="border: 0;padding-bottom: 10px;">
+                                                                        <p style="color: #000000; font-size: 13px; font-weight:600;">Details are Same as below : </p>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+
+                                                    <div class="movableContent" style="border: 0px; padding-top: 0px; position: relative;">
+                                                        <table width="100%" border="1" cellspacing="0" cellpadding="0" align="center" style="padding: 15px;background: #fff; border: 0;">
+
+                                                            <tbody>
+
+                                                                <tr style="border-bottom: 0;border-right: 1px solid grey;">
+                                                                    <td style="padding:15px; text-transform: capitalize;border-right: 0;">Name</td>
+                                                                    <td style="padding:15px;">[first-name] [last-name]</td>
+
+                                                                </tr>
+                                                                <tr style="    border-bottom: 0;    border-right: 1px solid grey;">
+                                                                    <td style="padding:15px; text-transform: capitalize;     border-right: 0;">Email</td>
+                                                                    <td style="padding:15px;">[your-email]</td>
+
+                                                                </tr>
+                                                                <tr style="    border-bottom: 0;    border-right: 1px solid grey;">
+                                                                    <td style="padding:15px; text-transform: capitalize;     border-right: 0;">Phone No.</td>
+                                                                    <td style="padding:15px;">[your-phone]</td>
+
+                                                                </tr>
+                                                                <tr style="    border-bottom: 1px solid gray;    border-right: 1px solid grey;">
+                                                                    <td style="padding:15px; text-transform: capitalize;     border-right: 0;">Message</td>
+                                                                    <td style="padding: 15px;">[textarea] </td>
+
+                                                                </tr>
+                                                                <tr>
+                                                                    <td style="border: 0;     padding: 25px 0 15px 0;">
+                                                                        <p style="color: #000000; font-size: 15px"><strong>Thanks,<br><a href="http://aircharter-international.com">Air Charter International</a> Team.</strong></p>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+
+
+                                                    <div class="movableContent-1 movableContent-footer" style="border: 0px; position: relative;">
+                                                        <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" style="background:#000000;padding: 25px 15px;">
+                                                            <tr>
+                                                                <td valign="top" align="center"><span>Copyright @ 2018 <a href="http://aircharter-international.com" style="text-decoration:none;">Air Charter International</a> All Right Reserved</span></td>
+
+                                                            </tr>
+
+                                                        </table>
+                                                    </div>
+
+
+                                                    <!--  =========================== The footer ===========================  -->
+                                                </td>
+                                                <td valign="top" width="20" class="spechide">&nbsp;</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+
+
+
+
+</body>
+
+</html>
